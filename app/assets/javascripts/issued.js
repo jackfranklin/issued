3207d20017c93ef88e75938b6dced309b305a7b8
Jack Franklin <jack@jackfranklin.net>
2013-09-11 21:54:29 +0100

    remove views

--- app/assets/javascripts/issued.js
+++ app/assets/javascripts/issued.js
@@ -1,7 +1,6 @@
 //= require ./store
 //= require_tree ./models
 //= require_tree ./controllers
-//= require_tree ./views
 //= require_tree ./helpers
 //= require_tree ./templates
 //= require ./router
